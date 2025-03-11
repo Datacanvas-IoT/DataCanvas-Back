31a8808b4ea9f8ad6603ff005e551c0ee4070fc8
Lakmina Pramodya Gamage <pramodyalakmina@gmail.com>
2025-03-11 16:45:23 +0530

    refactor: use environment variables for database configuration

--- db.js
+++ db.js
@@ -1,9 +1,9 @@
-const { Sequelize } = require('sequelize');
+const { Sequelize } = require('sequelize')
 require('dotenv').config();
 
-const sequelize = new Sequelize('iot-on-earth', 'datacanvasAdmin', '2215476LKRa', {
-  host: 'datacanvas-postgresql.postgres.database.azure.com',
-  port: 5432,
+const sequelize = new Sequelize(process.env.DB_NAME, process.env.DB_USER, process.env.DB_PASSWORD, {
+  host: process.env.DB_HOST,
+  port: process.env.DB_PORT,
   dialect: 'postgres',
   ssl: true,
   dialectOptions: {
@@ -16,7 +16,6 @@ const sequelize = new Sequelize('iot-on-earth', 'datacanvasAdmin', '2215476LKRa'
 
 });
 
-// Test the connection
 async function testConnection() {
   try {
     await sequelize.authenticate();
