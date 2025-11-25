fabc18060dccf4aef99663323ddde846ab25f0a7
lakminagamage <pramodyalakmina@gmail.com>
2025-11-25 23:47:39 +0530

    feat: implement Docker support and SSL configuration for database connection

--- db-docker.js
+++ db-docker.js
@@ -1,6 +1,7 @@
 const { Sequelize } = require('sequelize')
 require('dotenv').config();
 
+const isProduction = process.env.NODE_ENV === 'production';
 const useSSL = process.env.DB_USE_SSL === 'true' || false;
 
 const sequelizeOptions = {
